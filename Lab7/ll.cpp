18407bc5f59f62cc09b80344b3c0fdd9f15adeaa
nikeplusdash <nikeplusdash@gmail.com>
2019-09-24 16:02:49 +0530

    Lab8

--- Lab7/ll.cpp
+++ Lab7/ll.cpp
@@ -30,8 +30,9 @@ class ll {
 		Node* temp = new Node();
 		temp->data=n;
 		temp->next=NULL;
+		if(curr->data>n){temp->next=head;head=temp;return;}
 		while(curr->data<n&&curr->next!=NULL){prev=curr;curr=curr->next;}
-		if(curr->next==NULL){curr->next=temp;return;}
+		if(curr->next==NULL&&curr->data<n){curr->next=temp;return;}
 		temp->next=prev->next;
 		prev->next=temp;
 	}
@@ -40,14 +41,14 @@ class ll {
 		Node* temp = new Node();
 		temp->data=x;
 		temp->next=NULL;
+		if(head==NULL) return;
 		if(head->data==n){
 			temp->next=head;
 			head=temp;
-			std::cout << head;
 			return;
 		}
 		Node* curr = head;Node* prev=head;
-		while(curr->data!=n&&curr!=NULL){
+		while(curr!=NULL&&curr->data!=n){
 			prev=curr;
 			curr=curr->next;
 		}
@@ -58,10 +59,11 @@ class ll {
 
 	void insert_after(int x,int n){
 		Node* temp = new Node();
+		if(head==NULL) return;
 		temp->data=x;
 		temp->next=NULL;
 		Node* curr = head;
-		while(curr->data!=n&&curr!=NULL) {curr=curr->next;}
+		while(curr!=NULL&&curr->data!=n) {curr=curr->next;}
 		if(curr==NULL){return;}
 		temp->next=curr->next;
 		curr->next=temp;
@@ -69,48 +71,56 @@ class ll {
 
 	void del_alt(){
 		Node* curr = new Node();Node* prev=head;
-		int i=0;
-		curr = head;
+		curr = head->next;
 		while(curr!=NULL){
-			if(++i%2==0)	prev->next=curr->next;
-			prev=curr;
-			curr=curr->next;
+            Node* temp = curr;
+			prev->next=curr->next;
+            delete(temp);
+			prev=prev->next;
+			if(prev==NULL) return;
+			curr=prev->next;
 		}
-		delete(curr);
 	}
 
-	void delete_ll(int n){
-		Node* curr = new Node();Node* prev=head;
-		curr = head;
+	void delete_ll(int x){
+		if(head==NULL)  return;
+		Node* curr = head;
+		Node* prev = curr;
 		while(curr!=NULL){
-			if(curr->data==n) prev->next=curr->next;
-			prev=curr;
-			curr=curr->next;
+			if(head->data==x){Node* temp = curr;head=curr->next;curr=head;prev=curr;delete(temp);}
+			else if(curr->data==x){
+				Node* temp = curr;
+				prev->next = curr->next;
+				curr = prev->next;
+				delete(temp);
+				if(prev==NULL) return;
+			}
+			else {prev = curr;curr = curr->next;}
 		}
-		if(curr==NULL)return;
-		
-		delete(curr);
+		return;
 	}
 
 	void traverse(){
+		if(head==NULL)  return;
 		Node* curr = head;
-		while(curr!=NULL) {std::cout<< curr->data << " "; curr=curr->next;}
+		while(curr!=NULL) {std::cout << curr->data;if(curr->next!=NULL) std::cout << " -> ";curr = curr->next;}
 		std::cout << std::endl;
+		return;
 	}
 
-	void reverse(){
-		ll list;
-		Node* curr = head;
-		Node* temp =curr->next;
-		while(temp!=NULL) {
-			Node* next = temp->next;		
-			temp->next = curr;
-			curr = temp;
-			temp = next;
-		}
-		head->next=NULL;
-		head=curr;
-	}
+    void Reverse(){
+        if(head==NULL||head->next==NULL) return;
+        Node* prev = head;
+        Node* curr = head->next;
+        while(curr!=NULL){
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head->next = NULL;
+        head = prev;
+    }
 
 	void sort(){
 		Node* i=head;
@@ -130,19 +140,46 @@ class ll {
 };
 
 int main(){
-	ll linkedlist;
-	linkedlist.insert(4);linkedlist.traverse();
-	linkedlist.insert_after(7,4);linkedlist.traverse();
-	linkedlist.insert_before(2,7);linkedlist.traverse();
-	linkedlist.insert(4);linkedlist.traverse();
-	linkedlist.insert_after(7,4);linkedlist.traverse();
-	linkedlist.insert_before(2,7);linkedlist.traverse();
-	// linkedlist.delete_ll(2);linkedlist.traverse();
-	linkedlist.reverse();
-	linkedlist.sort();
-	linkedlist.traverse();
-	// linkedlist.del_alt();
-	linkedlist.insert_sorted(9);
-	linkedlist.insert_sorted(5);
-	linkedlist.traverse();
+	ll list;
+	while(1){
+		int ip,x,n;
+		std::cout<<"\n1:Insert n\n2:Insert before x\n3:Insert after x\n4:Delete n\n5:Display\n6:Reverse\n7:Sort\n8:Delete alternate\n9:Insert Sorted\n"<<std::endl;
+		std::cin>>ip;
+		switch(ip){
+			case 1: 
+                std::cin >> x;
+                list.insert(x);
+                break;
+            case 2:
+                std::cin >> x >> n;
+                list.insert_before(x,n);
+                break;
+            case 3:
+                std::cin >> x >> n;
+                list.insert_after(x,n);
+                break;
+            case 4:
+                std::cin >> x;
+                list.delete_ll(x);
+                break;
+            case 5:
+                list.traverse();
+                break;
+            case 6:
+                list.Reverse();
+                break;
+            case 7:
+                list.sort();
+                break;
+            case 8:
+                list.del_alt();
+                break;
+            case 9:
+                std::cin>>x;
+                list.insert_sorted(x);
+                break;
+            default:
+                return 0;
+		}
+	}
 }
