d07bff23407a042f34aafd886ff16b02897835d8
nikepludash <nikepludash@gmail.com>
2019-10-23 12:28:38 +0530

    5A updated

--- Lab11/src/lab11/Q5.java
+++ Lab11/src/lab11/Q5.java
@@ -4,37 +4,42 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class SyncMessage {
-    public synchronized void message(String msg){
-        System.out.println(msg);
+    public synchronized void message(int msg){
+        System.out.println("Table of "+msg+": ");
         try {
-            Thread.sleep(1000);
+            for(int i=1;i<=10;i++) {
+                System.out.println(msg*i);
+                Thread.sleep(200);
+            }
         } catch (InterruptedException ex) {
             Logger.getLogger(SyncMessage.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
 class Thread5 extends Thread {
+    int i;
     String msg;
     SyncMessage sm;
-    Thread5(int x,String s,SyncMessage sm){
+    Thread5(int x,String s,SyncMessage a){
         super(s);
-        this.sm = sm;
+        i=x;
+        sm=a;
         msg=s;
         System.out.println("Starting: "+this);
     }
     @Override
     public void run(){
-        sm.message(msg);
+        sm.message(i);
     }
 }
 public class Q5 {
     public static void main(String[] args) {
         SyncMessage sm = new SyncMessage();
-        Thread5 t1 = new Thread5(1,"Hi",sm);
-        Thread5 t2 = new Thread5(2,"How",sm);
-        Thread5 t3 = new Thread5(3,"are",sm);
-        Thread5 t4 = new Thread5(4,"you",sm);
-        Thread5 t5 = new Thread5(5,"?",sm);
+        Thread5 t1 = new Thread5(1,"Thread 1",sm);
+        Thread5 t2 = new Thread5(2,"Thread 2",sm);
+        Thread5 t3 = new Thread5(3,"Thread 3",sm);
+        Thread5 t4 = new Thread5(4,"Thread 4",sm);
+        Thread5 t5 = new Thread5(5,"Thread 5",sm);
         t1.start();
         t2.start();
         t3.start();
