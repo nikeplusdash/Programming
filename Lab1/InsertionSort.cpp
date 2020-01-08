6eb6b7204c6d4d80e20f7af8e3e4944e44aad2a6
nikeplusdash <nikeplusdash@gmail.com>
2020-01-08 10:03:25 +0530

    Removed debugging lines

--- Lab1/InsertionSort.cpp
+++ Lab1/InsertionSort.cpp
@@ -1,15 +1,11 @@
 #include<iostream>
 
 void InsertionSort(int a[],int n,int i) {
-    if(i == n) return;
-    
+    if(i == n) return;  
     int j = 0,k = a[i];
     for(j=i-1;j>-1&&a[j]>k;j--)
         a[j+1]=a[j];
     a[j+1]=k;
-    std::cout << "During Sorting: [ ";
-    for(int j=0;j<8;j++) std::cout << a[j] << " ";
-    std::cout << " ]" << std::endl;
     InsertionSort(a,n,++i);
 }
 
